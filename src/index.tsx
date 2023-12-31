6261e5ab978c31420d2ca5af2096f99a7bcd26a8
brecert <me@bree.dev>
2023-12-31 18:26:58 -0500

    add NotFound route

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,20 @@ import "./index.css";
 import { Root } from "./routes/root";
 import { Mod } from "./routes/mod/[id].tsx";
 import { mods } from "./routes/data.ts";
+import { NotFound } from "./routes/NotFound.tsx";
 
 const root = document.getElementById("root");
 
 render(
   () => (
     <Router preload={false}>
+      <Route path="/" component={Root} />
       <Route
         path="/mod/:namespace"
         component={Mod}
         matchFilters={{ namespace: mods.map((mod) => mod.namespace) }}
       />
-      <Route path="/" component={Root} />
+      <Route path="*" component={NotFound} />
     </Router>
   ),
   root!
